e92e78b56562c7d1719fc723e952bbe07441051b
VegaHidalgoLincolnJhoel <u23304682@utp.edu.pe>
2026-05-29 03:02:16 -0500

    mapping para matriculas

--- Pc2/src/main/java/com/javaScrpit/Pc2/repository/MatriculaRepository.java
+++ Pc2/src/main/java/com/javaScrpit/Pc2/repository/MatriculaRepository.java
@@ -2,6 +2,9 @@ package com.javaScrpit.Pc2.repository;
 
 import com.javaScrpit.Pc2.model.Matricula;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+    @Query("select m from Matricula m join fetch m.curso order by m.id desc")
+    List<Matricula> findAllWithCurso();
 }
